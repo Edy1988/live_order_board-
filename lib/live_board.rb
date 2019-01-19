8f561db7b14302d5fc649bd1c2dd33e51bd6efac
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-01-19 15:37:09 +0000

    Simplified register method to receive order instance instead of order data.

--- lib/live_board.rb
+++ lib/live_board.rb
@@ -7,12 +7,7 @@ class LiveBoard
     @all_orders = []
   end
 
-  def register(user_id:, quantity:, price_per_kg:, type:)
-    save(Order.new(user_id: user_id, quantity: quantity, price_per_kg: price_per_kg, type: type))
-  end
-
-  private
-  def save(order)
+  def register(order)
     @all_orders << order
   end
 
--- spec/live_board_spec.rb
+++ spec/live_board_spec.rb
@@ -7,25 +7,19 @@ describe 'LiveBoard' do
 
     expect(board.all_orders.length).to eq 0
   end
-  
+
   it 'stores registered orders' do
     board = LiveBoard.new
 
-    board.register(user_id: "user", quantity: 10, price_per_kg: 50, type: "SELL")
-    board.register(user_id: "user1", quantity: 1, price_per_kg: 10, type: "BUY")
+    order1 = Order.new(user_id: "user", quantity: 10, price_per_kg: 50, type: :sell)
+    order2 = Order.new(user_id: "user1", quantity: 1, price_per_kg: 10, type: :buy)
+
+    board.register(order1)
+    board.register(order2)
 
     expect(board.all_orders.length).to eq 2
-    expect(board.all_orders[0].user_id).to eq "user"
-    expect(board.all_orders[0].quantity).to eq 10
-    expect(board.all_orders[0].price_per_kg).to eq 50
-    expect(board.all_orders[0].type).to eq "SELL"
-
-    expect(board.all_orders.length).to eq 2
-    expect(board.all_orders[1].user_id).to eq "user1"
-    expect(board.all_orders[1].quantity).to eq 1
-    expect(board.all_orders[1].price_per_kg).to eq 10
-    expect(board.all_orders[1].type).to eq "BUY"
-
+    expect(board.all_orders[0]).to eq order1
+    expect(board.all_orders[1]).to eq order2
   end
 
 end
