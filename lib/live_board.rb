030b67b2c0227c1f5fd39a2c81d991437fca53e4
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-01-19 14:46:42 +0000

    Added register order method.

--- lib/live_board.rb
+++ lib/live_board.rb
@@ -1,8 +1,19 @@
+require 'order'
+
 class LiveBoard
   attr_reader :all_orders
 
   def initialize
     @all_orders = []
+  end
+
+  def register(user_id:, quantity:, price_per_kg:, type:)
+    save(Order.new(user_id: user_id, quantity: quantity, price_per_kg: price_per_kg, type: type))
+  end
+
+  private
+  def save(order)
+    @all_orders << order
   end
 
 end
